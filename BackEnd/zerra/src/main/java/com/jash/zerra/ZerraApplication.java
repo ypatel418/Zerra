c592bdf85bd9dc871e6a374359915a0b58b28f18
jakmills <jakem0822@outlook.com>
2026-01-04 12:49:12 -0500

    Inital Commit

---
+++ BackEnd/zerra/src/main/java/com/jash/zerra/ZerraApplication.java
@@ -0,0 +1,13 @@
+package com.jash.zerra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerraApplication.class, args);
+	}
+
+}
---
+++ BackEnd/zerra/src/test/java/com/jash/zerra/ZerraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jash.zerra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
