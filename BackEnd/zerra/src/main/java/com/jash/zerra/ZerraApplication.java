a087099a6c019812f7936ef999f2bd23f48f41ba
Yash Patel <ypatel41805@gmail.com>
2026-01-23 13:15:21 -0500

    added dotenv dependency so that it will get the variables from env file

--- BackEnd/zerra/src/main/java/com/jash/zerra/ZerraApplication.java
+++ BackEnd/zerra/src/main/java/com/jash/zerra/ZerraApplication.java
@@ -3,10 +3,17 @@ package com.jash.zerra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ZerraApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ZerraApplication.class, args);
 	}
 
