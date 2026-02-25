ff8a3745f5931f60bc78ff60ddc696167419c4b7
ypatel418 <ypatel41805@gmail.com>
2026-02-24 21:41:59 -0500

    removed dotenv stuff for deployment

--- BackEnd/zerra/src/main/java/com/jash/zerra/ZerraApplication.java
+++ BackEnd/zerra/src/main/java/com/jash/zerra/ZerraApplication.java
@@ -9,11 +9,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ZerraApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
 		SpringApplication.run(ZerraApplication.class, args);
 	}
 
