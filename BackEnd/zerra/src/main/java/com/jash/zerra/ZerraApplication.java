f910ad02fccc5aedfccefa42d7748fa574bc131e
ypatel418 <ypatel41805@gmail.com>
2026-03-12 17:22:35 -0400

    changed .env stuff. added a try catch block so deployment doesnt fail

--- BackEnd/zerra/src/main/java/com/jash/zerra/ZerraApplication.java
+++ BackEnd/zerra/src/main/java/com/jash/zerra/ZerraApplication.java
@@ -3,10 +3,20 @@ package com.jash.zerra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ZerraApplication {
 
 	public static void main(String[] args) {
+		try {
+			Dotenv dotenv = Dotenv.load();
+			System.setProperty("DB_URL", dotenv.get("DB_URL"));
+			System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		} catch (Exception e) {
+		}
+
 		SpringApplication.run(ZerraApplication.class, args);
 	}
 
