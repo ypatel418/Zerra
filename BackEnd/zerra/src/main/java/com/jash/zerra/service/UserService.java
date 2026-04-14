bbeaeb1f52e5a2acf9aee90f4b341ddeabea6cab
jakmills <jakem0822@outlook.com>
2026-04-14 08:29:56 -0400

    Increased file upload limit, added a bar that shows a user's storage amount, and a bar chart to show visually how much storage they have let

--- BackEnd/zerra/src/main/java/com/jash/zerra/service/UserService.java
+++ BackEnd/zerra/src/main/java/com/jash/zerra/service/UserService.java
@@ -1,9 +1,13 @@
 package com.jash.zerra.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jash.zerra.model.File;
 import com.jash.zerra.model.User;
+import com.jash.zerra.repo.FileRepo;
 import com.jash.zerra.repo.UserRepo;
 
 @Service
@@ -11,9 +15,33 @@ public class UserService {
 
     @Autowired
     private UserRepo repo;
+    @Autowired
+    private FileRepo fileRepo;
 
     public User newUser(User user) {
         System.out.println(user.getEmail());
         return repo.save(user);
+    }
+
+    public Long getUserStorageUsage(String userID) {
+        // Count all file bytes of the user and return it
+        List<File> files = fileRepo.findByOwnerId(userID);
+        Long storageUsage = 0L;
+
+        for (File file : files) {
+            if (file.getFileSize() != null) {
+                storageUsage += file.getFileSize();
+            }
+        }
+        return storageUsage;
+    }
+
+    public boolean userMaxStorageReached(String userID, File uploadedFile) {
+        // Max Storage at 100 mb
+        Long maxStorage = 100L * 1024L * 1024L;
+        Long currentStorageUsage = getUserStorageUsage(userID);
+        Long uploadedFileSize = uploadedFile.getFileSize() != null ? uploadedFile.getFileSize() : 0L;
+        
+        return (currentStorageUsage + uploadedFileSize) >= maxStorage;
     }
 }
